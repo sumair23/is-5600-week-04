422b83e29049d86666cf3b413316b2bbdf83a460
sumair23 <sumair.muhammad@slu.edu>
2024-11-20 05:39:44 +0000

    lab4 submission

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const fs = require('fs').promises
 const path = require('path')
 const express = require('express')
+const middleware = require('./middleware.js')
+const bodyParser = require('body-parser')
+
+
+// Add the api module
+const api = require('./api.js')
+// Require the middleware module
 
 // Set the port
 const port = process.env.PORT || 3000
@@ -8,9 +15,22 @@ const port = process.env.PORT || 3000
 const app = express()
 // Register the public directory
 app.use(express.static(__dirname + '/public'));
+//console.log(typeof middleware.cors)
+app.use(middleware.cors)
+app.use(bodyParser.json())
 // register the routes
-app.get('/products', listProducts)
-app.get('/', handleRoot);
+//app.get('/products', listProducts)
+//app.get('/', handleRoot);
+// update the route handlers
+// app.get('/', api.handleRoot)
+// app.get('/products', api.listProducts)
+// app.get('/products/:id', api.getProduct)
+app.get('/', api.handleRoot)
+app.get('/products', api.listProducts)
+app.get('/products/:id', api.getProduct)
+app.post('/products', api.createProduct)
+app.delete('/products/:id', api.deleteProduct)
+app.put('/products/:id', api.updateProduct)
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
@@ -30,6 +50,7 @@ function handleRoot(req, res) {
  */
 async function listProducts(req, res) {
   const productsFile = path.join(__dirname, 'data/full-products.json')
+
   try {
     const data = await fs.readFile(productsFile)
     res.json(JSON.parse(data))
